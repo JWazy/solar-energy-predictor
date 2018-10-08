43f30d69a85b164b98d4da7665f39705299208bd
Jeff Wasden <jeffwasden@gmail.com>
2018-10-08 08:53:29 -0600

    Added graphing capabilities (training+predictions)
    Added the ability to graph the metrics recorded during training (i.e.
    loss, accuracy, etc.). Also added the ability to graph the predictions vs
    the actual input based on whichever input data (i.e. y-axis: Solar Power
    Output   x-axis: Time (hrs)).

--- solar_energy_nn.py
+++ solar_energy_nn.py
@@ -66,9 +66,12 @@ TrainingSetFeatures = preprocessor(TrainingSetFeatures)
 #TrainingSetLabels = to_categorical(TrainingSetLabels, 5)
 scaleLabels(TrainingSetLabels)
 
+# init
+tflearn.init_graph(seed=1)
+
 #create a test set from the number of samples and traning set
 net = tflearn.input_data(shape=[None, 12])
-net = tflearn.fully_connected(net, 64)
+# net = tflearn.fully_connected(net, 64)
 net = tflearn.fully_connected(net, 32)
 net = tflearn.fully_connected(net, 16)
 #net = tflearn.fully_connected(net, 5, activation="softmax")
@@ -78,26 +81,42 @@ net = tflearn.regression(net, loss="mean_square")
 # categorized the data into bins for and that should be the number of 0.88888
 
 # Define model
-model = tflearn.DNN(net)
+model = tflearn.DNN(net, tensorboard_dir='C:/Users/jeffw/AppData/Local/Temp/tflearn_logs', tensorboard_verbose=2)
 
 # Start training (apply gradient descent algorithm)
 model.fit(TrainingSetFeatures, TrainingSetLabels, n_epoch=10, batch_size=12, show_metric=True, validation_set=0.1)
 
-# Let's create some data for DiCaprio and Winslet
-test = [[],[],[]]
-test[0] =  ["2/1/2016",6,0,9.92,0.37,-0.01,89.12,4.72,29.19,29.98,0]
-test[1] = ["7/18/2017",12,0,10,28.71,17.31,47.32,4.44,29.3,30.09,4568.75]
-test[2] = ["2/4/2016",8,0.3,10,-5.49,-8.8,74.57,11.88,29.46,30.26,1750.25]
+model.save('solar_prediction.model')
 
-test = preprocessor(test)
+import matplotlib.pyplot as plt
+numDataPoints = 100
+pred = model.predict(TrainingSetFeatures[0:numDataPoints])
+time = list(map(lambda x: x[1], data[0:numDataPoints]))
+scaledPred = list(map(lambda x: x[0] * 4750, pred))
+actualOutput = list(map(lambda x: x[0] * 4750, TrainingSetLabels[0:numDataPoints]))
+plt.plot(time, actualOutput, 'gs')
+plt.plot(time, scaledPred, 'ro')
+plt.title("Predicted vs. Actual Solar Power")
+plt.ylabel("Solar power")
+plt.xlabel("Time")
+plt.legend()
+plt.show()
 
-pred = model.predict(test)
+# # Let's create some data for DiCaprio and Winslet
+# test = [[],[],[]]
+# test[0] =  ["2/1/2016",6,0,9.92,0.37,-0.01,89.12,4.72,29.19,29.98,0]
+# test[1] = ["7/18/2017",12,0,10,28.71,17.31,47.32,4.44,29.3,30.09,4568.75]
+# test[2] = ["2/4/2016",8,0.3,10,-5.49,-8.8,74.57,11.88,29.46,30.26,1750.25]
 
-# find index
-lowOutputPrediction = pred[0] * 4750
-highOutputPrediction = pred[1] * 4750
-myOutputPrediction = pred[2] * 4750
+# test = preprocessor(test)
 
-print("Estimate:", lowOutputPrediction, "      Actual:", 0)
-print("Estimate:", highOutputPrediction, "      Actual:", 4568.75)
-print("Estimate:", myOutputPrediction, "      Actual:", 1750.25)
+# pred = model.predict(test)
+
+# # find index
+# lowOutputPrediction = pred[0] * 4750
+# highOutputPrediction = pred[1] * 4750
+# myOutputPrediction = pred[2] * 4750
+
+# print("Estimate:", lowOutputPrediction, "      Actual:", 0)
+# print("Estimate:", highOutputPrediction, "      Actual:", 4568.75)
+# print("Estimate:", myOutputPrediction, "      Actual:", 1750.25)
